dac23338255aa13d534d05da5d2c60b3aa8bdc80
karala <13047608002@163.com>
2017-09-07 15:44:02 +0800

    添加了对combineReducers的解释说明，说明了state是如何出来的

--- src/reducer.js
+++ src/reducer.js
@@ -36,6 +36,14 @@ const visibilityFilter = (state = SHOW_ALL, action) => {
     }
 };
 
+// combineReducers 辅助函数的作用是，把一个由多个不同 reducer 函数作为 value 的 object，合并成一个最终的 reducer 函数
+// 合并后的 reducer 可以调用各个子 reducer，并把它们的结果合并成一个 state 对象
+// state 对象的结构由传入的多个 reducer 的 key 决定
+// 最终，state 对象的结构会是这样的：
+// {
+//      reducer1: ...
+//      reducer2: ...
+// }
 const handle = combineReducers({
     todos,
     visibilityFilter
