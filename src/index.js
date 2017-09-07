8b1a67fba9b3d30c17cb2bca44b446e1bdf219b2
karala <13047608002@163.com>
2017-09-07 16:46:22 +0800

    添加了redux-devtools

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './container/index.jsx';
 import handle from './reducer.js';
 
-let store = createStore(handle);
+let store = createStore(
+    handle,
+    composeWithDevTools()
+);
 let rootElement = document.getElementById('root');
 
 render(
